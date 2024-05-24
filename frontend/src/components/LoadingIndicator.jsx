7076a0a9ccb71ccbe13d6c161d7e1a9035e12fdb
Tommy Moua <tmoua996@gmail.com>
2024-05-24 01:38:03 -0500

    Initial commit.

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,9 @@
+import "../styles/LoadingIndicator.css";
+
+const LoadingIndicator = () => {
+    return <div className="loading-indicator">
+        <div className="loader"></div>
+    </div>
+}
+
+export default LoadingIndicator
